5fef9713b4fc2511488806e7c114dcfeb1390332
JAY <JAY@oonne.com>
2025-01-16 11:39:43 +0800

    feat: 全局异常过滤器

--- src/common/filter/any-exception.filter.ts
+++ src/common/filter/any-exception.filter.ts
@@ -1,5 +1,8 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
 
+/*
+ * 全局异常过滤器
+ */
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
@@ -9,11 +12,18 @@ export class AllExceptionsFilter implements ExceptionFilter {
 
     const status =
       exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+    const message = exception instanceof Error ? exception.message : '服务器异常';
 
-    response.status(status).json({
+    // 此处需要打印错误日志
+    // TODO
+    console.log(request.url, message);
+
+    // 返回
+    response.status(status).send({
       statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
+      time: new Date().getTime(),
+      timestamp: new Date(),
+      message: message,
     });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filter/any-exception.filter';
 import { ValidationPipe } from './common/pipe/validate.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  // 全局异常过滤器（用于自定义异常）
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  // 全局管道
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(10011, '0.0.0.0');
 }
