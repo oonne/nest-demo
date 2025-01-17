4ea5ee005e17c241de20477374e6495f4a76693f
JAY <JAY@oonne.com>
2025-01-17 18:28:10 +0800

    feat: 新增常用utils

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,3 +1,7 @@
+/* 时间 */
+export type Timer = ReturnType<typeof setTimeout> | null;
+export type Interval = ReturnType<typeof setInterval> | null;
+
 /* HTTP接口默认返回数据格式 */
 export interface HttpResponse<T> {
   code: number;
---
+++ src/utils/utils.ts
@@ -0,0 +1,78 @@
+import CryptoJS from 'crypto-js';
+import type { Timer } from '../types/interface';
+
+/**
+ *  获取n位的数字随机数
+ */
+const randomDigits = (n: number): number => {
+  if (n > 21) {
+    return 0;
+  }
+  return Math.round((Math.random() + 1) * 10 ** (n - 1));
+};
+
+/**
+ *  获取n以内的随机整数
+ */
+const randomWithin = (n: number): number => Math.floor(Math.random() * n);
+
+/**
+ *  获取n位的随机数字或字母
+ */
+const randomChars = (n: number): string => {
+  const arr: string[] = [];
+  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+
+  for (let i = 0; i < n; i += 1) {
+    arr.push(chars[randomWithin(chars.length)]);
+  }
+
+  return arr.join('');
+};
+
+/*
+ * 获取哈希值
+ */
+const createHash = (text: string, maxLen?: number): string => {
+  const length = maxLen || 16;
+  let hashedText = CryptoJS.SHA256(text).toString();
+  if (hashedText.length > length) {
+    hashedText = hashedText.substring(0, length);
+  }
+  return hashedText;
+};
+
+/**
+ *  延迟一定时间，单位毫秒。
+ */
+const sleep = async (time: number): Promise<void> =>
+  new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+
+/**
+ * 函数防抖
+ */
+const debounce = <T extends (...args: any[]) => void>(fn: T, waitTime: number) => {
+  let timer: Timer = null;
+
+  return (...args: any[]) => {
+    if (timer !== null) {
+      clearTimeout(timer);
+    }
+
+    // 停止触发n秒后才执行
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, waitTime);
+  };
+};
+
+export default {
+  randomDigits,
+  randomWithin,
+  randomChars,
+  createHash,
+  sleep,
+  debounce,
+};
