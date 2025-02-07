0ecae604275c1ef5e8fa92c3867e75c4835d5cfa
JAY <JAY@oonne.com>
2025-02-07 19:54:40 +0800

    feat: auth

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,15 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  async login(loginDto: LoginDto) {
+  async login({ name, password }: { name: string; password: string }) {
     // 这里实现登录逻辑
     return {
-      access_token: 'mock_token', // 示例返回，实际应该实现 JWT 等token生成
-      user: {
-        username: loginDto.name,
-      },
+      name,
+      password,
     };
   }
 }
