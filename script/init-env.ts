06e9c52fe61cb65c044153796d0a80c46be88f3d
JAY <JAY@oonne.com>
2025-02-08 16:05:45 +0800

    feat: 初始化时随机生成秘钥

--- script/init-env.ts
+++ script/init-env.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-
+import * as crypto from 'crypto';
 const initEnv = () => {
   if (fs.existsSync('.env')) {
     console.log('已存在.env文件，无须初始化');
@@ -13,8 +13,14 @@ const initEnv = () => {
   // 读取.env.local文件内容
   let envLocalContent = fs.readFileSync('.env.local', 'utf8');
 
-  // 创建一个.env文件
+  // 创建一个.env文件，将ENV_NAME设置为prod
   envLocalContent = envLocalContent.replace('ENV_NAME = local', 'ENV_NAME = prod');
+  // 生成32位随机JWT_SECRET
+  envLocalContent = envLocalContent.replace(
+    /JWT_SECRET\s*=\s*.+/,
+    `JWT_SECRET = ${crypto.randomBytes(32).toString('hex')}`,
+  );
+  // 写入.env文件
   fs.writeFileSync('.env', envLocalContent);
 
   // 初始化完成
