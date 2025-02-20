efbfb41d2dedc59d0cf174a6f128624752c672f0
JAY <JAY@oonne.com>
2025-02-20 20:46:09 +0800

    chore: 测试脚本

---
+++ build-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 获取当前路径
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$SCRIPT_DIR"
+
+# 构建镜像
+docker-compose build
+echo "构建镜像完成"
+
+# 保存镜像
+docker save -o nest_demo_image.tar nest_demo
+echo "镜像已保存为 nest_demo_image.tar"
