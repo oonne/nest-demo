af09568e7c40933e158917171feb5d7093a04178
JAY <JAY@oonne.com>
2025-02-21 18:32:28 +0800

    devops: 脚本更新

--- build-image.sh
+++ build-image.sh
@@ -5,9 +5,10 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR"
 
 # 构建镜像
-docker-compose build
+docker compose build
 echo "构建镜像完成"
 
 # 保存镜像
+rm -rf nest_demo_image.tar
 docker save -o nest_demo_image.tar nest_demo
 echo "镜像已保存为 nest_demo_image.tar"
